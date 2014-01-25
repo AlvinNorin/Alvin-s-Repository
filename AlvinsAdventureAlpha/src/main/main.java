758df55dd1131e85b6ecf30cf9d21387f4798b6c
Alvin Norin <alvin.norin@gmail.com>
2014-01-25 22:44:27 +0100

    Change-Id: I19e9834ba3427e1b98e6f89bf1124fdacb6786b1

---
+++ AlvinsAdventureAlpha/src/main/main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author alvin
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+	}
+
+}
